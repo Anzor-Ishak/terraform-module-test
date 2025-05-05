25c254ec89c71e3cde6ce35f9cf5a515ba0def5a
Anzor Ishak <anzori@spacelift.io>
2025-05-05 11:25:54 -0400

    testing version examples

--- examples/caketest.tf
+++ examples/caketest.tf
@@ -1,4 +1,14 @@
 module "examples" {
   source  = "spacelift.io/anzor-ishak/test/module//examples"
   version = "0.0.1"
+
+  eggs  = 5
+  flour = "200g"
+}
+
+output "my-birthday-cake" {
+  value = {
+    weight    = module.my-birthday-cake.weight
+    allergens = module.my-birthday-cake.allergens
+  }
 }
