e38ad1af2efa2b6fcd74fb1e1ba6997055d750e1
Anzor Ishak <anzori@spacelift.io>
2025-05-30 11:29:53 -0400

    tf

--- provider.tf
+++ provider.tf
@@ -3,6 +3,14 @@ terraform {
     aws = {
       source  = "hashicorp/aws"
       version = "~> 5.96.0"
+
+
+      spacelift = {
+      source  = "spacelift-io/spacelift"
+      version = "~> 1.0" # Or a specific version like "1.14.0"
+    }
+  }
+}
     }
   }
 }
